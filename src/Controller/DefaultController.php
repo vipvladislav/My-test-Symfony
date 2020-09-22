ed04ecede72e295700e08dcb8e00f9dfeb080f25
vladislav <vipvladislav@gmail.com>
2020-09-22 16:45:54 +0300

    first work in symfony

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class DefaultController
+ * @package App\Controller
+ */
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/{aaa}/blog", name="blog_list")
+     */
+       public function index($aaa)
+    {
+        return $this->render('lucky/number.html.twig', ['number' => $aaa]);
+    }
+}
